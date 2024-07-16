0c52f1c9a317b421d3b886672ce49db0966dac8f
Aries Dmello <dmelloaries@gmail.com>
2024-07-17 00:08:55 +0530

    Added Pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
+
+import { Signup } from "./pages/Signup.jsx";
+import { Signin } from "./pages/Signin.jsx";
+import { Dashboard } from "./pages/Dashboard.jsx";
+import { SendMoney } from "./pages/SendMoney.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>Hello
-
-      </div>
+       <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/send" element={<SendMoney />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
